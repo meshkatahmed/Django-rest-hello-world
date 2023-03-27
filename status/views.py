af82883e4a1945a89f88399f2622c4b141a604bb
Meshkat Ahmed <meshkatuddinahammed@outlook.com>
2023-03-27 21:12:15 +0600

    Created a model serializer

---
+++ status/views.py
@@ -0,0 +1,12 @@
+from .models import Status
+from .serializers import StatusSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+class StatusAPIView(APIView):
+    def get(self,request,format=None):
+        status_list = Status.objects.all()
+        status_serializer = StatusSerializer(status_list,many=True)
+        return Response(status_serializer.data)
