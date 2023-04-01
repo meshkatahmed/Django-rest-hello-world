c4c4fbcc7a442a4287ca858611c9868e73eebd2a
Meshkat Ahmed <meshkatuddinahammed@outlook.com>
2023-04-01 22:14:23 +0600

    List & create API view

--- my_api/urls.py
+++ my_api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('status/', include('status.urls')),
+    path('apiv1/', include('status.urls')),
 ]
