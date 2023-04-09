3216ba79ebb8ff1b5af7e5744c15241a8c331f22
unknown <meshkatuddinahammed@outlook.com>
2023-04-09 14:39:51 +0600

    Compressed CRUD request

--- status/views.py
+++ status/views.py
@@ -4,39 +4,14 @@ from .serializers import StatusSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from rest_framework import generics
+from rest_framework import generics,mixins
 
 # Create your views here.
-class StatusAPIView(APIView):
-    def get(self,request,format=None):
-        status_list = Status.objects.all()
-        status_serializer = StatusSerializer(status_list,many=True)
-        return Response(status_serializer.data)
-
-class StatusListAPIView(generics.ListAPIView):
+class StatusListCreateAPIView(generics.ListCreateAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
 
-class StatusCreateAPIView(generics.CreateAPIView):
+class StatusDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Status.objects.all()
-    serializer_class = StatusSerializer
-
-class StatusDetailAPIView(generics.RetrieveAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
-    lookup_field = 'id'
-
-    # def get_object(self,*args,**kwargs):
-    #     kwargs = self.kwargs
-    #     kw_id = kwargs.get('id')
-    #     return Status.objects.get(id=kw_id)
-
-class StatusUpdateAPIView(generics.UpdateAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
-    lookup_field = 'id'
-
-class StatusDeleteAPIView(generics.DestroyAPIView):
-    queryset =Status.objects.all()
     serializer_class = StatusSerializer
     lookup_field = 'id'
